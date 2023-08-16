cd645152e65d5aa4a85d26ddd6908e7d0e22c30e
FarahAttili <farahalattili2242000@gmail.com>
2023-08-16 13:46:46 +0300

    Great updates

--- javaSpt/document.js
+++ javaSpt/document.js
@@ -6,14 +6,12 @@ function showList(e, divID) {
   } else {
    arrowImg = e.target.childNodes[1];
   }
-  //  let arrowImg= e.target.closest(".downArrow") || e.target.nextElementSibling;
  const isRotated = arrowImg.classList.contains("rotate")
   if (!isRotated) {
     arrowImg.classList.add("rotate");
   } else {
     arrowImg.classList.remove("rotate")
   }
-  
   let Id = divID.toString();
   let list = document.getElementById(Id);
   if (list.style.display === "none") {
@@ -22,51 +20,11 @@ function showList(e, divID) {
     list.style.display = "none";
   }
 }
-
-// function getCurrentYear() {}
-function showList(divID)
-{
-    let Id = divID.toString();
-    var list = document.getElementById(Id);
-    if (list.style.display === "none") {
-      list.style.display = "block";
-    } else {
-      list.style.display = "none";
-    }
-}
 function getCurrentYear()
 {
     const d = new Date();
     let year = d.getFullYear();
     document.getElementById("currentYear").innerHTML = year.toString();
-}
-function openTab(event, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("viewPage");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("navItem");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  event.currentTarget.className += " active";
-}
-
-function testfunc (path) {
-  document.getElementById("viewPage").href =path;
-}
-function loadContent(path) {
-  // console.log(path)
-  fetch(path)  // Fetch the content of content.html
-      .then(response => response.text())  // Convert the response to text
-      .then(data => {
-          document.getElementById('viewPage').innerHTML = data;  // Insert the content into the div
-      })
-      .catch(error => {
-          console.error('Error loading this page:', error);
-      });
 }
 //! Main Navigation Function
 function navigate(url) {
@@ -83,14 +41,9 @@ function navigate(url) {
     })
     .then(content => {
       container.innerHTML = content;
+      Prism.highlightAll();
     })
     .catch(error => {
       console.error(error);
     });
 }
-
-const mainFrame = document.getElementById("main-frame");
-
-// function navigate(path) {
-//   document.getElementById("main-frame").setAttribute('src', path);
-// }
